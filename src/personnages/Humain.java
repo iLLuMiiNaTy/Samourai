1036616d9affc139efa734fe0f60a9ff4c770705
NicolasRieuvernet <NicolasRieuvernet@Nicoww.private.univ-tlse3.fr>
2023-11-09 15:09:57 +0100

    class humain

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,5 +1,59 @@
 package personnages;
 
 public class Humain {
+	String nom;
+	int argent;
+	String boisson;
+	
 
+	public Humain(String nom, int argent, String boisson) {
+		this.nom = nom;
+		this.argent = argent;
+		this.boisson = boisson;
+	}
+	
+	
+	public void parler(String nom, String texte) {
+		System.out.println(this.nom + " - " + texte);
+	}
+	
+	
+	public void direBonjour (String nom, String boisson) {
+		String texte = "Bonjour ! Je m'appelle " + this.nom + 
+				" et j'aime boire du " + this.boisson;
+		parler(this.nom, texte);
+	}
+	
+	
+	public void boire (String nom, String boisson) {
+		String texte =  "Mmmm, un bon verre de " + this.boisson + " ! GLOUPS !";
+		parler(this.nom, texte);
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public int getArgent() {
+		return argent;
+	}
+
+
+	public String getBoisson() {
+		return boisson;
+	}
+	
+	
+	public void gagnerArgent(int montant) {
+		this.argent += montant;
+	}
+	
+	public void perdreArgent(int montant) {
+		if (this.argent <=0){
+			parler (this.nom, "J'ai les poches vides");
+		}
+		this.argent -= montant;
+	}
 }
